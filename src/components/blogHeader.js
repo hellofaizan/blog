c409d1d6ba957aef64013f507c6637770507f6ab
Mohammad Faizan <faizancurious@gmail.com>
2023-03-24 21:56:21 +0530

    Blogpost Better Meta Data

--- src/components/blogHeader.js
+++ src/components/blogHeader.js
@@ -8,8 +8,14 @@ function BlogHeader({ frontmatter }) {
             <title>{frontmatter.title} - HelloFaizan Blog</title>
             <meta name="description" content={frontmatter.description} />
             <meta name="author" content="Hello Faizan" />
-            <meta property="og:description" content={frontmatter.description}/>
+            <meta property="og:title" content={`${frontmatter.title} - HelloFaizan Blog`} />
+            <meta property="og:description" content={frontmatter.description} />
             <meta property="og:image" itemProp="image" content={frontmatter.bannerUrl} />
+            <meta property="og:type" content="article" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@HelloFaizandev" />
+            <meta name="twitter:creator" content="@HelloFaizandev" />
+            <meta name="twitter:title" content={`${frontmatter.title} - HelloFaizan Blog`} />
             <meta name="twitter:image" itemProp="image" content={frontmatter.bannerUrl} />
         </Head>
         <div>
