29d3104abb0c7e19941e9c32e2dfb7287277cc0c
Mohammad Faizan <faizancurious@gmail.com>
2023-03-24 12:45:54 +0530

    Added Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import Image from 'next/image'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 import { motion } from "framer-motion"
+import Link from 'next/link'
 
 const Home = () => {  
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    axios.get('https://api.npoint.io/4a8d62649d30ab3f091e', {
-      method: 'GET',
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, [data]);
 
   return (
     <>
+    <div className='mt-20 md:ml-16 ml-1'>
     <motion.div
-    initial={{ opacity: 0, scale: 0.8 }}
-    animate={{ opacity: 1, scale: 1 }}
+    initial={{ opacity: 0.8 }}
+    animate={{ opacity: 1 }}
     transition={{ duration: 0.2 }}>
-      <p className="text-3xl font-bold underline">
-        Hello World
-      </p>
+      <p className='font-mono text-yellow-400 mb-8'><Link href={"https://hellofaizan.me"}><i className='bi bi-browser-safari mr-2'></i>Looking for my portfolio?</Link></p>
+      <p className='text-lg font-mono'>👋 HelloFaizan Blog <Link href={"https://github.com/hellofaizan/blog"} target="_blank"><span className='hover:text-blue-500 text-gray-400'>- github</span></Link></p>
+      <p className='ml-2 text-gray-400 font-mono'>Everything that comes into my mind will be written here.</p>
     </motion.div>
+    </div>
     </>
   )
 }
